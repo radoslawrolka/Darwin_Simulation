c4c7de9db629861118ce09295315dfc49db94720
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-08-05 23:30:15 +0200

    Atomic fundaments

---
+++ src/main/java/agh/ics/oop/MapDirection.java
@@ -0,0 +1,47 @@
+package agh.ics.oop;
+
+public enum MapDirection {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    NORTH_WEST;
+
+    public MapDirection next() {
+        return switch(this) {
+            case NORTH -> NORTH_EAST;
+            case NORTH_EAST -> EAST;
+            case EAST -> SOUTH_EAST;
+            case SOUTH_EAST -> SOUTH;
+            case SOUTH -> SOUTH_WEST;
+            case SOUTH_WEST -> WEST;
+            case WEST -> NORTH_WEST;
+            case NORTH_WEST -> NORTH;
+        };
+    }
+
+    public MapDirection rotate(int angle) {
+        MapDirection result = this;
+        for (int i = 0; i < angle; i++) {
+            result = result.next();
+        }
+        return result;
+    }
+
+    public Vector2d unitVector() {
+        return switch(this) {
+            case NORTH -> new Vector2d(0, 1);
+            case NORTH_EAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH_EAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTH_WEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTH_WEST -> new Vector2d(-1, 1);
+        };
+    }
+}
+
