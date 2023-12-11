d276ff1526d7ca627f86d376637e2251c4727b06
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-11 15:39:03 +0100

    Genotypes created

--- src/main/java/agh/ics/oop/model/CrazyGenotype.java
+++ src/main/java/agh/ics/oop/model/CrazyGenotype.java
@@ -1,12 +1,20 @@
 package agh.ics.oop.model;
 
+import java.util.Random;
+
 public class CrazyGenotype extends Genotype{
     public CrazyGenotype(int[] parent1, int[] parent2, int energy1, int energy2) {
         super(parent1, parent2, energy1, energy2);
     }
 
     @Override
-    public int getGene() {
-        return 1;
+    public int getMove() {
+        if (new Random().nextInt(5) != 4) {
+            return super.getMove();
+        }
+        else {
+            this.current_gene = new Random().nextInt(this.genes.length);
+            return this.genes[this.current_gene];
+        }
     }
 }
