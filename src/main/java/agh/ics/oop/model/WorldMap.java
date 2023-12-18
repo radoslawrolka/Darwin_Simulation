81dbb7a447be2790780b5dfb42c30ef87f0d500c
WerWojtas <werwojtas@student.agh.edu.pl>
2023-12-18 12:08:32 +0100

    changed MoveDirection to MapDirection in move method

--- src/main/java/agh/ics/oop/model/WorldMap.java
+++ src/main/java/agh/ics/oop/model/WorldMap.java
@@ -22,7 +22,7 @@ public interface WorldMap<T, P> extends MoveValidator<P> {
      * Moves an animal (if it is present on the map) according to specified direction.
      * If the move is not possible, this method has no effect.
      */
-    void move(T object, MoveDirection direction);
+    void move(T object, MapDirection direction);
 
     /**
      * Return true if given position on the map is occupied. Should not be
