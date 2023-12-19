8a105991b629a57ae318de9e76cd88deb9eb7ba8
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-19 12:16:00 +0100

    implemented stats (getter todo)

---
+++ src/main/java/agh/ics/oop/model/Statistics.java
@@ -0,0 +1,55 @@
+package agh.ics.oop.model;
+
+public class Statistics {
+    private int children = 0;
+    private int descendants = 0;
+    private int daysAlive = 0;
+    private int plantsEaten = 0;
+    private final int dayOfBirth;
+    private int dayOfDeath = -1;
+    private Statistics parent1 = null;
+    private Statistics parent2 = null;
+
+    public Statistics(int dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public Statistics(Statistics parent1, Statistics parent2, int dayOfBirth) {
+        this.parent1 = parent1;
+        this.parent2 = parent2;
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    public void addDay() {
+        this.daysAlive++;
+    }
+
+    public void addPlant() {
+        this.plantsEaten++;
+    }
+
+    public void addChild() {
+        this.children++;
+        this.descendants++;
+        if (this.parent1 != null) {
+            this.parent1.addDescendant();
+        }
+        if (this.parent2 != null) {
+            this.parent2.addDescendant();
+        }
+    }
+
+    public void addDescendant() {
+        this.descendants++;
+        if (this.parent1 != null) {
+            this.parent1.addDescendant();
+        }
+        if (this.parent2 != null) {
+            this.parent2.addDescendant();
+        }
+    }
+
+    public void setDayOfDeath(int dayOfDeath) {
+        this.dayOfDeath = dayOfDeath;
+    }
+}
