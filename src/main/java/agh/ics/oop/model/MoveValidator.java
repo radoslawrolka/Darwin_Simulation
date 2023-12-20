3ccc308e0721c1ae176f76efaa11615f792dfe73
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-12-20 15:24:26 +0100

    refractor

--- src/main/java/agh/ics/oop/model/MoveValidator.java
+++ src/main/java/agh/ics/oop/model/MoveValidator.java
@@ -1,16 +1,6 @@
 package agh.ics.oop.model;
 
 public interface MoveValidator<P> {
-
-    /**
-     * Indicate if any object can move to the given position.
-     *
-     * @param position
-     *            The position checked for the movement possibility.
-     * @return True if the object can move to that position.
-     */
-    boolean canMoveTo(P position);
-
     /**
      * Return a position after moving in a given direction.
      *
