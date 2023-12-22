507bbfd99ccad36a1e027b8261919c2d84754f8a
WerWojtas <werwojtas@student.agh.edu.pl>
2023-12-22 20:24:33 +0100

    Jungle changes

--- src/main/java/agh/ics/oop/model/Jungle.java
+++ src/main/java/agh/ics/oop/model/Jungle.java
@@ -1,54 +1,99 @@
 package agh.ics.oop.model;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 public class Jungle implements GrassGrow {
 
-    private List<Vector2d> preferable = new LinkedList<>();
-    private List<Vector2d> not_preferable = new LinkedList<>();
+    private Map<Vector2d, Integer> preferable = new HashMap<>();
+    private Set<Vector2d> not_preferable = new HashSet<>();
 
     private Vector2d mapSize;
 
     public Jungle(Vector2d mapSize) {
         this.mapSize = mapSize;
-        IntStream.range(0, mapSize.getX() + 1)
-                .forEach(i ->
-                        IntStream.range(0, mapSize.getY() + 1)
-                                .forEach(j -> not_preferable.add(new Vector2d(i, j)))
-                );
+        for(int i=0; i<mapSize.getX(); i++){
+            for(int j=0; j<mapSize.getY(); j++){
+                not_preferable.add(new Vector2d(i, j));
+            }
+        }
     }
 
 
     @Override
     public void plantGrass(int grassNumber, HashMap<Vector2d, Grass> grasses) {
-        Random random = new Random();
-        for (int i = 0; i < grassNumber; i++) {
-            int size_preferable = preferable.size();
-            int size_not_preferable = not_preferable.size();
-            if (random.nextInt(5) != 0 && size_preferable > 0) {
-                int randomNumber1 = random.nextInt(size_preferable);
-                Vector2d position = preferable.get(randomNumber1);
-                grasses.put(position, new Grass(position));
-                preferable.remove(randomNumber1);//TODO: Collections.shuffle(preferable);
-                addPreferable(position, grasses);
-            } else if (size_not_preferable > 0) {
-                int randomNumber1 = random.nextInt(size_not_preferable);
-                Vector2d position = not_preferable.get(randomNumber1);
-                grasses.put(position, new Grass(position));
-                not_preferable.remove(randomNumber1);
-                addPreferable(position, grasses);
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        if (preferable.size() > 0 && not_preferable.size() > 0) {
+            for (int i = 0; i < grassNumber; i++) {
+                if (random.nextInt(5) != 0) {
+                    int randomIndex = random.nextInt(preferable.size());
+                    int k = 0;
+                    for (Vector2d position : preferable.keySet()) {
+                        if (k == randomIndex) {
+                            grasses.put(position, new Grass(position));
+                            preferable.remove(position);
+                            addPreferable(position, grasses);
+                            break;
+                        }
+                        k++;
+                    }
+                } else {
+                    int randomIndex = random.nextInt(not_preferable.size());
+                    int k = 0;
+                    for (Vector2d position : not_preferable) {
+                        if (k == randomIndex) {
+                            grasses.put(position, new Grass(position));
+                            not_preferable.remove(position);
+                            addPreferable(position, grasses);
+                            break;
+                        }
+                        k++;
+                    }
+                }
             }
-
         }
-
+        else if (preferable.size() > 0) {
+                for (int i = 0; i < grassNumber; i++) {
+                    int randomIndex = random.nextInt(preferable.size());
+                    int k = 0;
+                    for (Vector2d position : preferable.keySet()) {
+                        if (k == randomIndex) {
+                            grasses.put(position, new Grass(position));
+                            preferable.remove(position);
+                            addPreferable(position, grasses);
+                            break;
+                        }
+                        k++;
+                    }
+                }
+            } else if (not_preferable.size() > 0) {
+                for (int i = 0; i < grassNumber; i++) {
+                    int randomIndex = random.nextInt(not_preferable.size());
+                    int k = 0;
+                    for (Vector2d position : not_preferable) {
+                        if (k == randomIndex) {
+                            grasses.put(position, new Grass(position));
+                            not_preferable.remove(position);
+                            addPreferable(position, grasses);
+                            break;
+                        }
+                        k++;
+                    }
+                }
+            }
+        }
     }
 
-    private void addPreferable(Vector2d position, HashMap<Vector2d, Grass> grasses) {
+    private void addPreferable(Vector2d position, HashMap<Vector2d, Grass> grasses,) {
         for (MapDirection direction : MapDirection.values()) {
             Vector2d newPosition = position.add(direction.toUnitVector());
             if (checkAvailability(newPosition) && !grasses.containsKey(newPosition)) {
-                preferable.add(newPosition);
+                if (preferable.containsKey(newPosition)) {
+                    preferable.put(newPosition, preferable.get(newPosition) + 1);
+                } else {
+                    preferable.put(newPosition, 1);
+                }
             }
         }
 
@@ -64,17 +109,18 @@ public class Jungle implements GrassGrow {
     }
 
     @Override
-    public void eatGrass(List<Vector2d> eatenPositions, HashMap<Vector2d, Grass> grasses) {
-        for (Vector2d position : eatenPositions) {
-            grasses.remove(eatenPositions);
-            if (checkAvailability(position) && grasses.containsKey(position)) {
-                preferable.add(position);
-            }
-            else if (checkAvailability(position) && !grasses.containsKey(position)) {
-                not_preferable.add(position);
+    public void eatGrass(Vector2d eatenPosition, HashMap<Vector2d, Grass> grasses) {
+        grasses.remove(eatenPosition);
+        for (MapDirection direction : MapDirection.values()) {
+            Vector2d newPosition = eatenPosition.add(direction.toUnitVector());
+            if (checkAvailability(newPosition) && !grasses.containsKey(newPosition)) {
+                if (preferable.get(newPosition) > 1) {
+                    preferable.put(newPosition, preferable.get(newPosition) - 1);
+                } else {
+                    preferable.remove(newPosition);
+                }
             }
         }
-
     }
 }
 
