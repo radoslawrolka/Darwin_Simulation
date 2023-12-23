5641e254c85d6942a29e8ac8bbca5a2803c68f6d
WerWojtas <werwojtas@student.agh.edu.pl>
2023-12-23 13:41:15 +0100

    borders changed

--- src/main/java/agh/ics/oop/model/Borders.java
+++ src/main/java/agh/ics/oop/model/Borders.java
@@ -1,7 +1,14 @@
 package agh.ics.oop.model;
 
-public interface Borders {
-
-    void canMoveTo(Vector2d position);
-
+public interface Borders<P> {
+    /**
+     * Return a position after moving in a given direction.
+     *
+     * @param position
+     *            The initial position.
+     * @param UnitVector
+     *            The direction of the move.
+     * @return The final position after moving in the given direction.
+     */
+    P getPosition(P position, P UnitVector);
 }
