23e210c26288de85c84376697737dd90b56c6a58
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-01-06 13:07:56 +0100

    stats resolve descendant bug #14

--- src/main/java/agh/ics/oop/model/Statistics.java
+++ src/main/java/agh/ics/oop/model/Statistics.java
@@ -1,12 +1,14 @@
 package agh.ics.oop.model;
 
+import java.util.HashSet;
+
 public class Statistics {
     private int children = 0;
-    private int descendants = 0;
     private int daysAlive = 0;
     private int plantsEaten = 0;
-    private final int dayOfBirth;
     private int dayOfDeath = -1;
+    private final int dayOfBirth;
+    private final HashSet<Integer> descendantsSet = new HashSet<>();
     private Statistics parent1 = null;
     private Statistics parent2 = null;
 
@@ -18,8 +20,8 @@ public class Statistics {
         this.parent1 = parent1;
         this.parent2 = parent2;
         this.dayOfBirth = dayOfBirth;
-        this.parent1.addChild();
-        this.parent2.addChild();
+        this.parent1.addChild(this.hashCode());
+        this.parent2.addChild(this.hashCode());
     }
 
     public void addDay() {
@@ -30,18 +32,18 @@ public class Statistics {
         this.plantsEaten++;
     }
 
-    private void addChild() {
+    private void addChild(int hashChild) {
         this.children++;
-        addDescendant();
+        addDescendant(hashChild);
     }
 
-    private void addDescendant() {
-        this.descendants++;
+    private void addDescendant(int hashDescendant) {
+        this.descendantsSet.add(hashDescendant);
         if (this.parent1 != null) {
-            this.parent1.addDescendant();
+            this.parent1.addDescendant(hashDescendant);
         }
         if (this.parent2 != null) {
-            this.parent2.addDescendant();
+            this.parent2.addDescendant(hashDescendant);
         }
     }
 
@@ -53,7 +55,7 @@ public class Statistics {
         return this.children;
     }
     public int getDescendants() {
-        return this.descendants;
+        return this.descendantsSet.size();
     }
     public int getDaysAlive() {
         return this.daysAlive;
